e11392c3d2360dfd166319eb25f57763b551bce0
Shakeel Mohamed <smohamed@splunk.com>
2013-12-22 13:51:22 -0800

    Some mocha testing w/ pg

--- test/tests.js
+++ test/tests.js
@@ -10,15 +10,26 @@ describe('Array', function(){
     	})
   	})
 });
-describe('pg', function() {
+
+describe('PostgreSQL', function() {
+	describe('DATABASE_URL test', function() {
+		it('should fail when DATABASE_URL is not set', function() {
+			assert.notEqual(process.env.DATABASE_URL, undefined, 'DATABASE_URL is undefined');
+		});
+	});
+
 	describe('create user table', function() {
-		it('should successfully create the user table', function() {
+		it('should fail when unable to create the user table', function() {
+			var entered = false;
 			pg.connect(process.env.DATABASE_URL, function (err, client) {
+				entered = true;
 				assert.ifError(err);
+				assert.equal(true, fs.existsSync('databases/users.sql'));
 				client.query(fs.readFileSync('databases/user.sql'), function (err, result) {
 					assert.ifError(err);
 				});
 			});
+			assert.equal(true, entered, 'PostgreSQL connection failed.');
 		})
 	})
 });
