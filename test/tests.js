e90b589129c92f9953183ef73aee3e054fe96758
Shakeel Mohamed <smohamed@splunk.com>
2013-12-22 19:40:08 -0800

    Fix spacing.

--- test/tests.js
+++ test/tests.js
@@ -3,46 +3,44 @@ var pg = require("pg");
 var fs = require("fs");
 
 describe('Array', function(){
-	describe('#indexOf()', function(){
-		it('should return -1 when the value is not present', function(){
-      		assert.equal(-1, [1,2,3].indexOf(5));
-      		assert.equal(-1, [1,2,3].indexOf(0));
-    	})
-  	})
+    describe('#indexOf()', function(){
+        it('should return -1 when the value is not present', function(){
+              assert.equal(-1, [1,2,3].indexOf(5));
+              assert.equal(-1, [1,2,3].indexOf(0));
+        })
+      })
 });
 
 describe('PostgreSQL', function() {
-	describe('DATABASE_URL test', function() {
-		it('should fail when DATABASE_URL is not set', function() {
-			console.log("DATABASE_URL: ["+process.env.DATABASE_URL+"]");
-			assert.notEqual(process.env.DATABASE_URL, undefined, 'DATABASE_URL is undefined');
-			assert.equal(process.env.DATABASE_URL, 'postgres://postgres:@127.0.0.1/nopejs_test');
-		});
-	});
+    describe('DATABASE_URL test', function() {
+        it('should fail when DATABASE_URL is not set', function() {
+            console.log("DATABASE_URL: ["+process.env.DATABASE_URL+"]");
+            assert.notEqual(process.env.DATABASE_URL, undefined, 'DATABASE_URL is undefined');
+            assert.equal(process.env.DATABASE_URL, 'postgres://postgres:@127.0.0.1/nopejs_test');
+        });
+    });
 
-	describe('create user table', function() {
-		it('should fail when unable to create the user table', function() {
-			var entered = false;
-			//the next line makes no sense to me.
-			pg.connect('', function(err,client){});
-			pg.connect(process.env.DATABASE_URL, function (err, client) {
-				//TODO: this code is never being executed
-				console.log("We connected just fine.");
-				entered = true;
-				console.log("We're in the connect block, right? : ", entered);
+    describe('create user table', function() {
+        it('should fail when unable to create the user table', function() {
+            var entered = false;
+            pg.connect(process.env.DATABASE_URL, function (err, client) {
+                //TODO: this code is never being executed
+                console.log("We connected just fine.");
+                entered = true;
+                console.log("We're in the connect block, right? : ", entered);
 
-				assert.ifError(err);
+                assert.ifError(err);
 
-				assert.equal(true, fs.existsSync('databases/users.sql'), 'users.sql exists');
+                assert.equal(true, fs.existsSync('databases/users.sql'), 'users.sql exists');
 
-				client.query(fs.readFileSync('databases/user.sql'), function (err, result) {
-					assert.ifError(err);
-					console.log("We ran the query, I guess.");
-				});
-				client.end();
-			});
-			//console.log("How about now? : ", entered);
-			//assert.equal(true, entered, 'PostgreSQL connection failed.');
-		})
-	})
+                client.query(fs.readFileSync('databases/user.sql'), function (err, result) {
+                    assert.ifError(err);
+                    console.log("We ran the query, I guess.");
+                });
+                client.end();
+            });
+            //console.log("How about now? : ", entered);
+            //assert.equal(true, entered, 'PostgreSQL connection failed.');
+        })
+    })
 });
