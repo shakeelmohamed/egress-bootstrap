816d04817b80de90b5c20863fb4d5aae6788a8d1
Shakeel Mohamed <smohamed@splunk.com>
2013-12-22 21:27:07 -0800

    new stuff

--- test/tests.js
+++ test/tests.js
@@ -23,12 +23,11 @@ describe('PostgreSQL', function() {
 
     describe('create user table', function() {
         it('should fail if unable to connect to database', function(done) {
-            this.timeout(15000);
             //process.env.DATABASE_URL = 'postgres://postgres:@127.0.0.1/nopejs_test';
             var nope_client = new pg.Client('postgres://postgres:@127.0.0.1/nopejs_test');
-            //assert.equal(nope_client.user, 'postgres'); //For some reason, client.user is smohamed
-            console.log(nope_client);
-            //nope_client.end();
+            assert.equal(nope_client.user, 'postgres'); //For some reason, client.user is smohamed
+            
+            nope_client.end();
             done();
         });
         it('should fail when unable to create the user table', function() {
