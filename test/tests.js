007279cfce476537f81bd8d80872cdc035eb9175
Shakeel Mohamed <smohamed@splunk.com>
2013-12-22 21:25:18 -0800

    GIVE.ME.OUTPUT

--- test/tests.js
+++ test/tests.js
@@ -65,13 +65,15 @@ describe('PostgreSQL', function() {
               if(err) {
                 return console.error('error fetching client from pool', err);
               }
+              console.log("SOMETHING?");
               nopejs_client.query('SELECT $1::int AS numbor', ['1'], function(err, result) {
                 //call `done()` to release the client back to the pool
                 done();
-
+                console.log("SOMETHING, ANYTHING......?");
                 if(err) {
                   return console.error('error running query', err);
                 }
+                console.log("SOMETHING, ANYTHING...... PLZzzzzz?");
                 console.log(result.rows[0].numbor);
                 //output: 1
               });
