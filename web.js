8479d79ce2a3d71fd7a85601e085587bafb9947a
Shakeel Mohamed <smohamed@splunk.com>
2013-12-25 00:51:01 -0800

    Fix indenting, simplified web.js, remove express.bodyParser warnings

--- web.js
+++ web.js
@@ -1,31 +1,32 @@
 var express = require('express'),
-	jade = require('jade'),
-	pg = require('pg'),
-	app = express(),
+    jade = require('jade'),
+    pg = require('pg'),
+    app = express(),
     routes = require('./routes');
 
 app.configure(function () {
+    app.locals({
+        sitename: 'Nope.JS',
+        author: 'Shakeel Mohamed'
+    });
+    app.locals.pretty = true;
     app.set('views', __dirname + '/jade');
     app.set('view engine', 'jade');
-    app.locals.pretty = true;
-    app.use(express.bodyParser());
+    app.use(express.urlencoded());
+    app.use(express.json());
     app.use(express.cookieParser());
-	app.use(express.session({ secret: 'Nope.js'}));
+    app.use(express.session({ secret: 'Nope.js'}));
     app.use(express.static(__dirname + '/public'));
     app.use(app.router);
     app.use(function(req, res, next){
-		res.redirect('/404');
-	});
+        res.redirect('/404');
+    });
 });
 
-var config = {
-    sitename: 'Nope.JS',
-    author: 'Shakeel Mohamed'
-};
-routes.init(app, config);
+routes.init(app);
 
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
-	console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
