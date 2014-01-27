27aff65771dba6cc81c57dcefe29aaccb887dc96
Shakeel Mohamed <smohamed@splunk.com>
2014-01-26 23:06:06 -0800

    Cleanup

--- test/bcrypt.js
+++ test/bcrypt.js
@@ -2,7 +2,7 @@ module.exports = function(testuser, bcrypt, assert){
     describe("Bcrypt", function(){
         describe("Test user's password", function() {
             it("should match the encrypted password", function() {
-                assert( bcrypt.compareSync(testuser.password, bcrypt.hashSync(testuser.password)) == true );
+                assert( bcrypt.compareSync(testuser.password, bcrypt.hashSync(testuser.password)) );
             });
         });
     });
