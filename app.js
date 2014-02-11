2e2d8bdd888ea40e074b3677fcd91e3800b7814c
Shakeel Mohamed <smohamed@splunk.com>
2014-02-10 22:29:09 -0800

    did some stuff

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+//var async = require("async");
 var express = require("express");
 var jade = require("jade");
 var pg = require("pg");
-var routes = require("./routes");
 
-var app = module.exports = express();
+var routes = require("./routes");
+var config = require("./config");
+var app = express();//= module.exports = express();
 
 app.configure(function () {
-    app.locals(require("./config")); // Get site configuration from config.js
+    app.locals(config); // Get site configuration from config.js
     app.locals.pretty = true;
     app.set("views", __dirname + "/jade");
     app.set("view engine", "jade");
@@ -22,3 +24,36 @@ app.configure(function () {
 });
 
 routes.init(app);
+
+pg.connect(config.postgres, function (err, client) {
+    if (err) {
+        return console.error("could not connect to postgres", err);
+    }
+    
+    var checkTableQuery = "select * from information_schema.tables where table_name='users'";
+
+    client.query(checkTableQuery, function (err, result){
+        if (err) {
+            console.log("The users table doesn't exist.", err);
+
+            var createSQL = fs.readFileSync("databases/users.sql", "utf8");
+
+            client.query(createSQL, function (err, result) {
+                if (err) {
+                    console.log("ERROR on creating users table:", err);
+                }
+                else {
+                    console.log("Created the users table.");
+                }
+            });
+        }
+        else {
+            console.log("The users table already exists.");
+        }
+    });
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
--- config.js
+++ config.js
@@ -1,7 +1,54 @@
+var fs = require("fs");
+var url = require("url");
+/*
 var config = {
     "siteName": "Egress",
     "siteAuthor": "Shakeel Mohamed",
     "siteDescription": "Egress, start writing a web app already."
 };
+*/
+function getDictOfEnvVars(filepath) {
+    var fileOfVariable = fs.readFileSync(filepath, "utf8").split("\n");
+    var envsDict = {};
+    fileOfVariable.forEach(function(variable){
+        var v = variable.split("=");
+        if(v && v.length==2 && v[0] && v[1])
+            envsDict[v[0].replace(" ", "")] = v[1];
+    });
+
+    //TODO: instead, make this copy to a dummy variable
+    var parsedPostgresURL = url.parse(envsDict.DATABASE_URL);
+    //envsDict.postgres = url.parse(envsDict.DATABASE_URL);
+
+    var auth = parsedPostgresURL.auth.split(":");
+    envsDict.postgres = {};
+    envsDict.postgres.user = auth[0];
+    envsDict.postgres.password = auth[1];
+
+    envsDict.postgres.database = parsedPostgresURL.path.replace("/", "");
+    envsDict.postgres.port = parseInt(parsedPostgresURL.port);
+    envsDict.postgres.host = parsedPostgresURL.hostname;
+    /*
+    delete envsDict.postgres.protocol;
+    delete envsDict.postgres.slashes;
+    delete envsDict.postgres.hash;
+    delete envsDict.postgres.auth;
+    delete envsDict.postgres.search;
+    delete envsDict.postgres.query;
+    delete envsDict.postgres.path;
+    delete envsDict.postgres.pathname;
+    delete envsDict.postgres.href;
+    delete envsDict.postgres.hostname;
+    */
+
+    if(!envsDict.PGSSLMODE || envsDict.PGSSLMODE != false)
+        envsDict.postgres.ssl = true;
+    return envsDict;
+}
+
+var config = getDictOfEnvVars(".env");
+config.siteName = "Egress";
+config.siteAuthor = "Shakeel Mohamed";
+config.siteDescription = "Egress, start writing a web app already.";
 
 module.exports = config;
